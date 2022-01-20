64700bed59f2194e168b85564af9165dc783dd91
Alberto <albgar290@alu.centroafuera.es>
2022-01-20 23:52:43 +0100

    ¿Acabado?

--- src/main/kotlin/com/example/demo/Quiz.kt
+++ src/main/kotlin/com/example/demo/Quiz.kt
@@ -2,13 +2,9 @@ package com.example.demo
 
 import com.google.gson.Gson
 
-
-data class Quiz (var identificador : Int,var pregunta: String, var respuestas : MutableList<String>,var respuestaCorrecta:String){
+data class Quiz (var identificador : Int,var pregunta: String, var respuestas : List<String>,var respuestaCorrecta:String){
     override fun toString(): String {
         val gson = Gson()
         return gson.toJson(this)
     }
-
-
-
 }
--- src/main/kotlin/com/example/demo/Repositorio.kt
+++ src/main/kotlin/com/example/demo/Repositorio.kt
@@ -2,17 +2,17 @@ package com.example.demo
 
 class Repositorio {
     companion object{
-        var preguntasYrespuestas= mutableListOf(
-            Quiz(1,"Capital de España?", mutableListOf("Madrid","Roma","Londres"),"Madrid"),
-            Quiz(2,"Capital de Reino Unido?", mutableListOf("Madrid","Canberra","Londres"),"Londres"),
-            Quiz(3,"Capital de Italia?", mutableListOf("Madrid","Roma","Nom_Pen"),"Roma"),
-            Quiz(4,"Capital de Alemania?", mutableListOf("Berlin","Roma","Bogota"),"Berlin"),
-            Quiz(5,"Capital de Belgica?", mutableListOf("Bruselas","Roma","Londres"),"Bruselas"),
-            Quiz(6,"Capital de Australia?", mutableListOf("Bruselas","Roma","Canberra"),"Canberra"),
-            Quiz(7,"Capital de Iraq?", mutableListOf("Bagdad","Roma","San Jose"),"Bagdad"),
-            Quiz(8,"Capital de Ecuador?", mutableListOf("Quito","Roma","Londres"),"Quito"),
-            Quiz(9,"Capital de Peru?", mutableListOf("Madrid","Bagdad","Lima"),"Lima"),
-            Quiz(10,"Capital de Marruecos?", mutableListOf("Bogota","Rabat","Londres"),"Rabat")
+        val preguntasYrespuestas= listOf(
+            Quiz(1,"Capital de España?", listOf("Madrid","Roma","Londres"),"Madrid"),
+            Quiz(2,"Capital de Reino Unido?", listOf("Madrid","Canberra","Londres"),"Londres"),
+            Quiz(3,"Capital de Italia?", listOf("Madrid","Roma","Nom_Pen"),"Roma"),
+            Quiz(4,"Capital de Alemania?", listOf("Berlin","Roma","Bogota"),"Berlin"),
+            Quiz(5,"Capital de Belgica?", listOf("Bruselas","Roma","Londres"),"Bruselas"),
+            Quiz(6,"Capital de Australia?", listOf("Bruselas","Roma","Canberra"),"Canberra"),
+            Quiz(7,"Capital de Iraq?", listOf("Bagdad","Roma","San Jose"),"Bagdad"),
+            Quiz(8,"Capital de Ecuador?", listOf("Quito","Roma","Londres"),"Quito"),
+            Quiz(9,"Capital de Peru?", listOf("Madrid","Bagdad","Lima"),"Lima"),
+            Quiz(10,"Capital de Marruecos?", listOf("Bogota","Rabat","Londres"),"Rabat")
         )
     }
 }
