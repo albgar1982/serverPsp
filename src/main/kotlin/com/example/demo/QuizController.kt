143505a4b125995b24b1d149d22b6ad4b1a85a16
Alberto <garciadepablos82@gmail.com>
2022-02-10 00:05:14 +0100

    Supuestamente acabada entrega token

--- src/main/kotlin/com/example/demo/QuizController.kt
+++ src/main/kotlin/com/example/demo/QuizController.kt
@@ -10,63 +10,99 @@ class QuizController(private val userRepository: UserRepository) {
 
     @GetMapping("solicitarPregunta/{token}")
     fun getQuestion(@PathVariable token: String): String {
-        val usuario = encontrarUsuario(token)
+        var pregunta = ""
+        val usuario = userRepository.getById(encontrarUsuario(token))
         val tokenCaducado = checkTokenTime(usuario)
         if (tokenCaducado) {
             println("ERROR")
             return "ERROR"
         } else {
-            var indice = Random.nextInt(
-                0,
-                Repositorio.preguntasYrespuestas.size
-            )
-            usuario?.let {
-                it.preguntas?.let {
-                    while(!it.contains(Repositorio.preguntasYrespuestas[indice].identificador))
-                        indice = Random.nextInt(0,Repositorio.preguntasYrespuestas.size)
-                }
-            }
+            if(usuario.idPreguntas.size==0)
+                return "No quedan preguntas"
+            val numPregunta = usuario.idPreguntas[Random.nextInt(0, usuario.idPreguntas.size)]
+            var i = 0
+            var salir = false
+            do {
+                if (Repositorio.preguntasYrespuestas[i].identificador == numPregunta) {
+                    salir = true
+                    pregunta = Repositorio.preguntasYrespuestas[i].toString()
+                    usuario.idPreguntas.remove(numPregunta)
+                    userRepository.save(usuario)
+                } else
+                    i++
+            } while (!salir && i < Repositorio.preguntasYrespuestas.size)
 
-            val enJson = Repositorio.preguntasYrespuestas[indice].toString()
-            usuario?.let {
-                it.preguntas?.remove(Repositorio.preguntasYrespuestas[indice].identificador)
-            }
-            val objetoJson = JSONObject(enJson)
+
+            val objetoJson = JSONObject(pregunta)
             objetoJson.remove("respuestaCorrecta")
             println(objetoJson)
             return objetoJson.toString()
         }
     }
 
-    @GetMapping("responder/{id}/{respuesta}")
-    fun answer(@PathVariable id: Int, @PathVariable respuesta: Int): String {
-        var aDevolver = "Identificador de pregunta incorrecto"
-        var i = 0
-        var salir = false
-        do {
-            if (Repositorio.preguntasYrespuestas[i].identificador == id) {
-                salir = true
-                aDevolver = if (Repositorio.preguntasYrespuestas[i].respuestaCorrecta == respuesta)
-                    "Respuesta correcta"
-                else
-                    "Respuesta incorrecta"
-            } else
-                i++
-        } while (!salir && i < Repositorio.preguntasYrespuestas.size)
+    @GetMapping("responder/{id}/{respuesta}/{token}")
+    fun answer(@PathVariable id: Int, @PathVariable respuesta: Int, @PathVariable token: String): String {
+        val usuario = userRepository.getById(encontrarUsuario(token))
+        val tokenCaducado = checkTokenTime(usuario)
+        if (tokenCaducado) {
+            println("ERROR")
+            return "ERROR"
+        } else {
+            var aDevolver = "Identificador de pregunta incorrecto"
+            var i = 0
+            var salir = false
+            do {
+                if (Repositorio.preguntasYrespuestas[i].identificador == id) {
+                    salir = true
+                    aDevolver = if (Repositorio.preguntasYrespuestas[i].respuestaCorrecta == respuesta)
+                        "Respuesta correcta"
+                    else
+                        "Respuesta incorrecta"
+                } else
+                    i++
+            } while (!salir && i < Repositorio.preguntasYrespuestas.size)
 
-        return aDevolver
+            return aDevolver
+        }
     }
 
     @GetMapping("appendUser/{usuario}/{contrasenia}")
     fun appendUser(@PathVariable usuario: String, @PathVariable contrasenia: String): String {
-        var token = crearToken()
+        val token = crearToken()
         val fecha = Calendar.getInstance()
-        val user = User(usuario, contrasenia, token, fecha)
-        userRepository.save(user)
+        val posibleUsuario = comprobarUsuarioYcontrasenia(usuario, contrasenia)
+        if (posibleUsuario != null) {
+            //Ya existe el usuario
+            posibleUsuario.token = token
+            posibleUsuario.fecha = fecha
+            userRepository.save(posibleUsuario)
+        } else {
+            val user = User(usuario, contrasenia, token, fecha, generaListaIdsPreguntas())
+            userRepository.save(user)
+        }
         userRepository.findAll().forEach {
             println(it)
         }
         return token
+    }
+
+    private fun comprobarUsuarioYcontrasenia(usuario: String, contrasenia: String): User? {
+        var user: User? = null
+        val listaUsuarios = userRepository.findAll()
+        var i = 0
+        var salir = false
+        do {
+            if(listaUsuarios.isEmpty())
+                salir = true
+            else{
+                if (listaUsuarios[i].usuario == usuario && listaUsuarios[i].contrasenia == contrasenia) {
+                    user = listaUsuarios[i]
+                    salir = true
+                } else
+                    i++
+            }
+        } while (!salir && i < listaUsuarios.size)
+        return user
     }
 
     private fun crearToken(): String {
@@ -92,19 +128,31 @@ class QuizController(private val userRepository: UserRepository) {
         return caducado
     }
 
-    private fun encontrarUsuario(token: String): User? {
-        var usuario: User? = null
+    private fun encontrarUsuario(token: String): Int {
+        var idUsuario = -1
         val listaUsuarios = userRepository.findAll()
         var i = 0
         var salir = false
         do {
             if (listaUsuarios[i].token == token) {
-                println("Estoy aquí")
-                usuario = listaUsuarios[i]
+                println("He encontrado el token $token. El del usuario es: ${listaUsuarios[i].token}")
+                idUsuario = listaUsuarios[i].id
                 salir = true
-            } else
+            } else{
+                println("NO he encontrado el token $token. El del usuario es: ${listaUsuarios[i].token}")
                 i++
+            }
+
         } while (!salir && i < listaUsuarios.size)
-        return usuario
+        return idUsuario
+    }
+
+    private fun generaListaIdsPreguntas(): ArrayList<Int> {
+        val listaPreguntas = arrayListOf<Int>()
+
+        Repositorio.preguntasYrespuestas.forEach {
+            listaPreguntas.add(it.identificador)
+        }
+        return listaPreguntas
     }
 }
--- src/main/kotlin/com/example/demo/User.kt
+++ src/main/kotlin/com/example/demo/User.kt
@@ -3,30 +3,18 @@ package com.example.demo
 import com.google.gson.Gson
 import java.util.*
 import javax.persistence.*
-
+import kotlin.collections.ArrayList
 
 @Entity
-data class User(var usuario:String, var contrasenia:String, var token:String, var fecha:Calendar) {
-
+data class User(var usuario:String, var contrasenia:String, var token:String, var fecha:Calendar,var idPreguntas: ArrayList<Int>) {
 
     @Id
     @GeneratedValue
     var id= 0
-
-    @Column
-    @ElementCollection(targetClass = Int::class)
-    var preguntas:MutableList<Int>? =generaListaPreguntas()
 
     override fun toString(): String {
         val gson = Gson()
         return gson.toJson(this)
     }
 
-    private fun generaListaPreguntas(): MutableList<Int>? {
-        val listaPreguntas :MutableList<Int>? = null
-        Repositorio.preguntasYrespuestas.forEach { pregunta ->
-            listaPreguntas?.add(pregunta.identificador)
-        }
-        return listaPreguntas
-    }
 }
